0947236647df5a5c8212e0cdf793bcc7300dc9d7
Rorchach <cookmycode@gmail.com>
2014-08-20 22:33:35 +0800

    haha, livereload go!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,39 @@
 module.exports = function(grunt) {
+
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
 	// Project configuration
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            options: {
+                port: 9000,
+                hostname: '*', //默认就是这个值，可配置为本机某个 IP，localhost 或域名
+                livereload: 35729  //声明给 watch 监听的端口
+            },
+ 
+            server: {
+                options: {
+                    open: true, //自动打开网页 http://
+                    base: [
+                        '.'  //主目录
+                    ]
+                }
+            }
+        },
 		// 通过watch任务，来监听文件是否有更改
 		watch: {
-			client: {
-				// 我们不需要配置额外的任务， watch任务已经内建LiveReload浏览器刷新的代码片段。
-				options: {
-					livereload: true
-				},
-				// ’**‘ 表示包含所有的子目录(此处没有用到)
-				// '*' 表示包含所以的文件
-				files: ['*.html', '*.css', '*.js']
-			}
+			livereload: {
+                options: {
+                    livereload: '<%=connect.options.livereload%>'  //监听前面声明的端口  35729
+                },
+ 
+                files: [  //下面文件的改变就会实时刷新网页
+                    '*.html',
+                    '*.css',
+                    '*.js'
+                ]
+            }
 		}
 	});
 
@@ -20,5 +41,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// 自定义任务
+	 grunt.registerTask('serve', [
+        'connect:server',
+        'watch'
+    ]);
 
 }
